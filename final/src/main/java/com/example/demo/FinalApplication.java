97b61dc53ede434091ab84cccd7c819a95990ae5
hjmin <hjmin@LAPTOP-OIEH0VJ7>
2019-11-09 19:51:03 +0900

    init

---
+++ final/src/main/java/com/example/demo/FinalApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalApplication.class, args);
+	}
+	
+	@GetMapping(value = "/")
+	public String hello () {
+		return "hello!";
+	}
+	
+	
+
+}
