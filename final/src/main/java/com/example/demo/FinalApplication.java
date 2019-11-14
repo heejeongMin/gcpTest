11ff0350b6853a70ef030c5a8862767d1b84690e
hjmin <hjmin@LAPTOP-OIEH0VJ7>
2019-11-14 21:03:54 +0900

    ssl?

--- final/src/main/java/com/example/demo/FinalApplication.java
+++ final/src/main/java/com/example/demo/FinalApplication.java
@@ -31,16 +31,15 @@ public class FinalApplication {
 	      }
 	    };
 	   
-	  tomcat.addAdditionalTomcatConnectors(redirectConnector());
+	 // tomcat.addAdditionalTomcatConnectors(redirectConnector());
 	  return tomcat;
 	}
 	 
 	private Connector redirectConnector() {
 	  Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-	  connector.setScheme("http");
-	  connector.setPort(8080);
-	  connector.setSecure(false);
-	  connector.setRedirectPort(8005);
+//	  connector.setScheme("http");
+//	  connector.setSecure(false);
+	
 	   
 	  return connector;
 	}
