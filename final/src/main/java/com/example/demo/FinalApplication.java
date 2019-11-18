31d60779efadefc10fdfdec4c33e684af68ed98e
hjmin <hjmin@LAPTOP-OIEH0VJ7>
2019-11-18 22:09:07 +0900

    8080 8443 주석

--- final/src/main/java/com/example/demo/FinalApplication.java
+++ final/src/main/java/com/example/demo/FinalApplication.java
@@ -39,8 +39,8 @@ public class FinalApplication {
 	  Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 	  connector.setScheme("http");
 	  connector.setSecure(false);
-	  connector.setPort(8080);
-	  connector.setRedirectPort(8443);
+//	  connector.setPort(8080);
+//	  connector.setRedirectPort(8443);
 	   
 	  return connector;
 	}
